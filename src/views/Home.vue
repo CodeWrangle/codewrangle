c2fba1be8aab84986529b3183b0e1a5b79f9b388
Brian Kennedy <bpkennedy@gmail.com>
2020-12-25 16:03:28 -0600

    Make order of tags uniform and remove comments

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,7 @@
-<template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-    <div v-if="prerenderBuilt">Seeing this text is proof that app prerendering happened!</div>
-  </div>
-</template>
-
 <script>
-// @ is an alias to /src
 import { PRERENDER_KEY } from '../../config'
 
 export default {
-  name: 'Home',
   components: {
     HelloWorld: () => import('../components/HelloWorld.vue')
   },
@@ -37,3 +27,11 @@ export default {
   }
 }
 </script>
+
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div v-if="prerenderBuilt">Seeing this text is proof that app prerendering happened!</div>
+  </div>
+</template>
